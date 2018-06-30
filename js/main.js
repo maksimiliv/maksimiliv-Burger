110a919f103ed4fbafef05eea80459a0e34d8fba
maksimiliv <35606748+maksimiliv@users.noreply.github.com>
2018-07-01 00:33:28 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -28,7 +28,7 @@ function initMap() {
     map: map,
     title: 'MrBurger',
     icon: {
-      url: "icons/map-marker.svg",
+      url: "icons/map-marker.png",
       scaledSize: new google.maps.Size(64, 64)
     }
   });
